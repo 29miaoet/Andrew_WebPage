51298f318009acf5708226ed76b443518b1df36d
29miaoet <29miaoet@users.noreply.github.com>
2026-05-12 13:33:31 +0000

    chore: version update

--- js/file_download.js
+++ js/file_download.js
@@ -86,10 +86,10 @@ document.getElementById("downloadBtn2").addEventListener("click", (e) => {
 
 // Source code download, zip archive.
 document.getElementById("downloadBtn3").addEventListener("click", (e) => {
-    downloadFile("https://github.com/29miaoet/Andrew_WebPage/archive/refs/tags/v28.1.2.zip");
+    downloadFile("https://github.com/29miaoet/Andrew_WebPage/archive/refs/tags/v28.1.3.zip");
 });
 
 // Source code download, tarball archive.
 document.getElementById("downloadBtn4").addEventListener("click", (e) => {
-    downloadFile("https://github.com/29miaoet/Andrew_WebPage/archive/refs/tags/v28.1.2.tar.gz");
+    downloadFile("https://github.com/29miaoet/Andrew_WebPage/archive/refs/tags/v28.1.3.tar.gz");
 });
